4255b79266b563b98ac54b2444373c15576f514a
a3rzi <amirhosseinqatar@gmail.com>
2024-01-12 21:07:20 +0330

    init project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //given
+        //An input of 765
+        int n = 765;
+
+        //when
+        //will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
+         int result =  squareDigits(n);
+
+        System.out.println("Result after squaring digits: " + result);
+    }
+
+    private static int squareDigits(int n) {
+        String num2str = String.valueOf(n);
+
+        for (int i =0; i<num2str.length(); i++)
+            //later wii finish
+
+        return 0;
+    }
+
+
+}
