d05c753e8155c7a5b778ec674fe2c0c1ab96212b
a3rzi <amirhosseinqatar@gmail.com>
2024-01-15 12:25:59 +0330

    String.valueOf
    StringBuilder
    Character.getNumericValue
    toString()
    Integer.parseInt

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,13 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //given
-        //An input of 765
-        int n = 765;
+        System.out.println("Enter a number: ");
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
 
         //when
         //will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
@@ -15,15 +18,18 @@ public class Main {
 
     private static int squareDigits(int n) {
         String num2str = String.valueOf(n);
+        StringBuilder resultstr = new StringBuilder();
+        int str2int = 0;
+
 
         for (int i = 0; i < num2str.length(); i++) {
             char num = num2str.charAt(i);
             int num2int = Character.getNumericValue(num);
             int squaredDigit = num2int* num2int;
-
-            return 0;
+            resultstr.append(squaredDigit);
+            String stringBuiler2String = resultstr.toString();
+            str2int = Integer.parseInt(stringBuiler2String);
         }
-        return n;
+        return str2int;
     }
-
 }
--- src/test/java/FunctionTest.java
+++ src/test/java/FunctionTest.java
@@ -16,4 +16,49 @@ public class FunctionTest {
         int i = Character.getNumericValue(a);
         System.out.println(i*i);
     }
+
+    @Test
+    void process() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        int n = 765;
+        String n2str = String.valueOf(n);
+        System.out.println("string"+n2str);
+        for (int i = 0; i< n2str.length(); i++){
+            char str2char = n2str.charAt(i);
+            System.out.println("char" + str2char);
+            int char2int = Character.getNumericValue(str2char);
+            System.out.println("int"+ char2int);
+            int squared = char2int*char2int;
+            System.out.println("squar"+squared);
+            stringBuilder.append(squared);
+            System.out.println(stringBuilder);
+
+        }
+    }
+
+    @Test
+    void stringBuilder() {
+        StringBuilder stringBuilder = new StringBuilder("welcome");
+        stringBuilder.append(20);
+//        stringBuilder.append(" to");
+//        stringBuilder.append(" iran");
+//        stringBuilder.insert(3,"java");
+ //       stringBuilder.replace(2,4,"abd");
+ //       stringBuilder.delete(2,4);
+ //       stringBuilder.reverse();
+        System.out.println(stringBuilder);
+    }
+
+    @Test
+    void toSTRING() {
+        Integer n = 5;
+        String n2string = n.toString();
+    }
+
+    @Test
+    void valueOF() {
+        int n = 10;
+        String n2string = String.valueOf(n);
+    }
 }
